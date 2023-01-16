52caca0c4b951cf8fa7cae95747d6b9fd66d87b7
Jorge Rivas <97417231+J0rgeR1vas@users.noreply.github.com>
2023-01-16 01:59:08 +0000

    sticky header js and css

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
+///text to search code
+function search() {
+  let textToSearch = document.getElementById("text-to-search").value;
+  let paragraph = document.getElementById("paragraph");
+  textToSearch = textToSearch.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
 
-function search(){
-    let textToSearch = document.getElementById("text-to-search").value;
-    let paragraph = document.getElementById("paragraph");
-    textToSearch = textToSearch.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+  let pattern = new 
+  RegExp(`\\b${textToSearch}\\b`,"gi");
 
-    let pattern = new RegExp(`${textToSearch}`,"gi");
-
-    paragraph.innerHTML = paragraph.textContent.replace(pattern, match => `<mark>${match}</mark>`)
+  paragraph.innerHTML = paragraph.textContent.replace(pattern, match => `<mark>${match}</mark>`)
 }
+
+///sticky header code 
+
+window.onscroll = function () {myFunction()};
+
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+
